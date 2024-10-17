2976f8ad7d7d1263217035301986fc70692a01fc
JHyun12864 <hyeon12864@naver.com>
2024-10-17 19:21:22 +0900

    1ST

--- ColumnarTranspositiionCipher.py
+++ ColumnarTranspositiionCipher.py
@@ -3,5 +3,7 @@
 # 암호키 입력 받기(중복되는 단어가 없어야 함.)
 key = "BRAIN"
 
-for i in range(len(key)):
-    test = key[i]
+for i in range(len(key)-1):
+    for j in range(int(i+1),len(key)):
+        if(key[i] == key[j]):
+            print("중복된 단어가 있습니다. \n키 값을 다시 입력해주세요.")
