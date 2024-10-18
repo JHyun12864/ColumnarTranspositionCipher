c040e499a15ae234e8aaa8ef350e41f458bd2c89
JHyun12864 <hyeon12864@naver.com>
2024-10-18 17:40:58 +0900

    2

--- ColumnarTranspositiionCipher.py
+++ ColumnarTranspositiionCipher.py
@@ -9,4 +9,27 @@ for i in range(len(key)-1):
             print("중복된 단어가 있습니다. \n키 값을 다시 입력해주세요.")
 
 # 펑문 입력 받기
-# 암호키의 순서 = 평문을 배열하는 열의 개수(table 이용할 예정)
+PLAINTEXT = "ABCDEFGHIJKLMNOPQRSTVWXYZ"
+
+# 암호키의 알파벳에 순서 부여(dictionary 이용)
+# sorted 함수를 이용해서 알파벳 정렬(dictionary key 이용)
+key_dic = {}
+sorted_key = sorted(key)
+for i in range(1, len(key)+1):
+    key_dic[i] = sorted_key[i-1]
+
+
+this_keydic = {}
+for i in range(len(key)):
+    for j in range(1, len(key)+1):
+      if(key[i] == key_dic[j]):
+            this_keydic[i+1] = key_dic[j]
+
+print(this_keydic)
+
+
+# 암호키의 길이 = 평문을 배열하는 열의 개수(list? table? 이용할 예정)
+
+# row, column = 2,3
+# arr_2d = [[None]*column for i in range(row)]
+# print(arr_2d)
